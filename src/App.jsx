820869b2457168e94d1323103553a1aa18bc47bf
William Kwadwo Owusu <williamowusu1998@gmail.com>
2025-05-29 10:55:57 +0000

    First commit to setup react project with shadcn for money transfer project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+     <h1 className='text-2xl text-center'>Hello There!</h1>
+     <Button>Click Me</Button>
+    </>
+  )
+}
+
+export default App

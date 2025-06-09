7eaafc93936cca3b52385399a784e8debf1adcaa
asare-develops <139370967+asare-develops@users.noreply.github.com>
2025-06-09 12:28:14 +0000

    Login screen
    Worked on login screen

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -4,7 +4,6 @@ import HeroSection from "@/components/HeroSection";
 import RatesSection from "@/components/RatesSection";
 import MobileMoneySection from "@/components/MobileMoney";
 import TestimonialSection from "@/components/Testimonial";
-import Login from "@/components/Login";
 
 export default function LandingPage() {
   return (
